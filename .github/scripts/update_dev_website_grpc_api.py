60f1af041d72a75e9587c4f07f34cdf116dcb6aa
Tharik Kanaka <tharik.kanaka@gmail.com>
2023-01-23 17:05:52 +0530

    Handle proto file changes

--- .github/scripts/update_dev_website_grpc_api.py
+++ .github/scripts/update_dev_website_grpc_api.py
@@ -4,6 +4,6 @@ from common_utils import update_md
 def main():
     update_md('grpc_api/grpc_api.bal', 'grpc-api.md',
               'update-grpc-api', 'gRPC API')
-
-
+    update_md('grpc_api/descriptor_record_store.proto', 'grpc-api.md',
+              'update-grpc-api', 'gRPC API')
 main()
